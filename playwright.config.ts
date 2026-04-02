0bcad3627028a059c57a23871b18d191d0d83061
Nikita <nekitzt2006@gmail.com>
2026-04-02 17:03:41 +0300

    Fix lab4 task4 try5

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,33 +1,18 @@
 import { defineConfig, devices } from '@playwright/test';
 
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 export default defineConfig({
-  // Переконайся, що папка з файлом app.spec.ts називається саме 'tests'
   testDir: './tests', 
-  
-  /* Run tests in files in parallel */
   fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* ФІКС: Встановлюємо базовий URL, щоб Playwright знав куди заходити */
     baseURL: 'http://localhost:3000',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-  },
+  }, 
 
-  /* Configure projects for major browsers */
   projects: [
     {
       name: 'chromium',
@@ -41,8 +26,7 @@ export default defineConfig({
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
     },
-  ],
-
+  ], 
 
   webServer: {
     command: 'npm run dev',       
