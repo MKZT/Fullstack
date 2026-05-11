75cd191bd99b4031492151ae0ac09d971c15001f
MKZT <126704350+MKZT@users.noreply.github.com>
2026-05-11 19:10:44 +0300

    js code ( Курсова робота )

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log("Dashboard Loaded");
+
+const cards = document.querySelectorAll(".event-card");
+
+cards.forEach(card => {
+
+    card.addEventListener("mouseenter", () => {
+
+        card.style.transform = "translateY(-10px)";
+
+    });
+
+    card.addEventListener("mouseleave", () => {
+
+        card.style.transform = "translateY(0px)";
+
+    });
+
+});
